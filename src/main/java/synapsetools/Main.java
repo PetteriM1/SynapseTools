c62353c06ac4c5afa4c64c35474ce41677b3439b
PetteriM1 <petteri.malkavaara1@gmail.com>
2019-04-02 19:21:12 +0300

    Initial commit

---
+++ src/main/java/synapsetools/Main.java
@@ -0,0 +1,67 @@
+package synapsetools;
+
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerQuitEvent;
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.Config;
+
+import org.itxtech.synapseapi.SynapsePlayer;
+
+import java.util.List;
+import java.util.Random;
+
+public class Main extends PluginBase implements Listener {
+
+    Config c;
+
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        saveDefaultConfig();
+        c = getConfig();
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof SynapsePlayer) {
+            SynapsePlayer p = (SynapsePlayer) sender;
+            if (command.getName().equalsIgnoreCase("transfer")) {
+                if (c.getBoolean("transferCommandEnabled")) {
+                    if (args.length > 0) {
+                        if (p.getSynapseEntry().getServerDescription().equalsIgnoreCase(args[0])) {
+                            p.sendMessage("\u00A7cYou are already on this server");
+                        } else {
+                            p.transferByDescription(args[0]);
+                        }
+                    } else {
+                        p.sendMessage("Usage: /transfer <target>");
+                    }
+                }
+            } else if (command.getName().equalsIgnoreCase("hub") || command.getName().equalsIgnoreCase("lobby")) {
+                if (c.getBoolean("hubCommandEnabled")) {
+                    List<String> l = c.getStringList("lobbiesForThisServer");
+                    if (l.size() == 0) {
+                        p.sendMessage("\u00A7cThis feature isn't configured correctly, please contact administrator");
+                        return true;
+                    }
+                    if (!l.contains(p.getSynapseEntry().getServerDescription())) {
+                        p.transferByDescription(l.get(new Random().nextInt(l.size())));
+                    } else {
+                        p.sendMessage("\u00A7cYou are already on a lobby server");
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        if (!c.getBoolean("hideSynapseMessages")) return;
+        String message = e.getQuitMessage().toString();
+        if (message.equals("timeout") || message.equals("generic reason") || message.equals("client disconnect") || message.equals("unknown")) {
+            e.setQuitMessage("");
+        }
+    }
+}
