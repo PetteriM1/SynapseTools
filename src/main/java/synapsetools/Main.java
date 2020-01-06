83aefa7f2e48f0843c42fe36676734f10ecd45d4
Petteri <petteri.malkavaara1@gmail.com>
2020-01-06 14:31:05 +0200

    Hide effects on transfer

--- src/main/java/synapsetools/Main.java
+++ src/main/java/synapsetools/Main.java
@@ -7,17 +7,20 @@ import cn.nukkit.command.CommandSender;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerQuitEvent;
+import cn.nukkit.network.protocol.MobEffectPacket;
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.potion.Effect;
 import cn.nukkit.utils.Config;
 
 import org.itxtech.synapseapi.SynapsePlayer;
+import org.itxtech.synapseapi.event.player.SynapsePlayerTransferEvent;
 
 import java.util.List;
-import java.util.Random;
+import java.util.SplittableRandom;
 
 public class Main extends PluginBase implements Listener {
 
-    Config c;
+    private Config c;
 
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
@@ -41,23 +44,22 @@ public class Main extends PluginBase implements Listener {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof SynapsePlayer) {
             SynapsePlayer p = (SynapsePlayer) sender;
-            if (command.getName().equalsIgnoreCase("transfer")) {
+            String n = command.getName();
+            if (n.equalsIgnoreCase("transfer") || n.equalsIgnoreCase("srv")) {
                 if (c.getBoolean("transferCommandEnabled")) {
                     if (args.length > 0) {
                         if (p.getSynapseEntry().getServerDescription().equals(args[0])) {
                             p.sendMessage("\u00A7cYou are already on this server");
-                        } else {
-                            if (!p.transferByDescription(args[0])) {
-                                p.sendMessage("\u00A7cUnknown server");
-                            }
+                        } else if (!p.transferByDescription(args[0])) {
+                            p.sendMessage("\u00A7cUnknown server");
                         }
-                    } else {
-                        p.sendMessage("Usage: /transfer <target>");
+
+                        return true;
                     }
-                } else {
-                    return false;
                 }
-            } else if (command.getName().equalsIgnoreCase("hub") || command.getName().equalsIgnoreCase("lobby")) {
+
+                return false;
+            } else if (n.equalsIgnoreCase("hub") || n.equalsIgnoreCase("lobby")) {
                 if (c.getBoolean("hubCommandEnabled")) {
                     List<String> l = c.getStringList("lobbiesForThisServer");
                     if (l.size() == 0) {
@@ -65,7 +67,7 @@ public class Main extends PluginBase implements Listener {
                         return true;
                     }
                     if (!l.contains(p.getSynapseEntry().getServerDescription())) {
-                        p.transferByDescription(l.get(new Random().nextInt(l.size())));
+                        p.transferByDescription(l.get(new SplittableRandom().nextInt(l.size())));
                     } else {
                         p.sendMessage("\u00A7cYou are already on a lobby server");
                     }
@@ -84,6 +86,21 @@ public class Main extends PluginBase implements Listener {
         String message = e.getQuitMessage().toString();
         if (message.equals("timeout") || message.equals("generic reason") || message.equals("client disconnect") || message.equals("unknown")) {
             e.setQuitMessage("");
+        }
+    }
+
+    @EventHandler
+    public void onTransfer(SynapsePlayerTransferEvent e) {
+        if (!e.isCancelled()) {
+            if (!c.getBoolean("removeEffectsOnTransfer")) return;
+            SynapsePlayer p = e.getPlayer();
+            for (Effect ef : p.getEffects().values()) {
+                MobEffectPacket pk = new MobEffectPacket();
+                pk.eid = p.getId();
+                pk.effectId = ef.getId();
+                pk.eventId = MobEffectPacket.EVENT_REMOVE;
+                p.dataPacket(pk);
+            }
         }
     }
 }
